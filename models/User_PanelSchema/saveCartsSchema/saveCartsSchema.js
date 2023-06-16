7ce03c610dda22e392ef6a68364d8fedac5022eb
nare25753 <charannarendra656@gmail.com>
2023-06-16 18:14:41 +0530

    updated code

--- models/User_PanelSchema/saveCartsSchema/saveCartsSchema.js
+++ models/User_PanelSchema/saveCartsSchema/saveCartsSchema.js
@@ -1,18 +1,22 @@
 const mongoose=require("mongoose")
 
 const schema=new mongoose.Schema({
-    firstName:{
+    cardNumber:{
+        type:Number,
+        require:true
+    },
+    cardHolderName:{
         type:String,
         require:true
     },
-    lastName:{
-        type:String,
-        require:true
-    },
-    cardType:{
-        type:[String],
-        require:true
-    }
+  validTime:{
+    type:String,
+    require:true
+  },
+  cvv:{
+    type:Number,
+    require:true
+  }
 })
 schema.set("timestamps",true)
 module.exports=mongoose.model("saveCarts",schema)
