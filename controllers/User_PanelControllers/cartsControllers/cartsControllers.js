bc4bdb98029f4ce4e7450404ccbf8b990c50822b
nare25753 <charannarendra656@gmail.com>
2023-12-05 10:10:17 +0530

    done

--- controllers/User_PanelControllers/cartsControllers/cartsControllers.js
+++ controllers/User_PanelControllers/cartsControllers/cartsControllers.js
@@ -290,20 +290,18 @@ exports.coupanDetails = async (req, res) => {
 exports.orderSummery = async (req, res) => {
   try {
     const id = req.params.id;
-    let carts = await cartSchema
-      .find({ user_Id: id })
-      .populate("product_Id");
+    let carts = await cartSchema.find({ user_Id: id }).populate("product_Id");
     const cartsTotal = carts.map((cartsTotal) => cartsTotal.products);
     const shipping = 40;
     const Tax = 30;
     // var cartsTotalSum = parseInt(cartsTotal) + shipping + Tax;
     const product = await cartSchema
       .find({ user_Id: id })
-     .populate("product_Id");
+      .populate("product_Id");
     res.status(200).json(
       success(res.statusCode, "Success", {
-        //   product,
-        cartsTotal,
+        product,
+        //  cartsTotal,
         // shipping,
         // Tax,
         // cartsTotalSum,
