1b93f94c1693d4cfe9dd8fc624271edf1ffb7216
jagriti89 <jagritisinghpro@gmail.com>
2023-12-27 10:33:58 +0530

    done

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ app.use(cors());
 app.use(bodyparser.json());
 app.use(morgan("dev"));
 app.set("view engine", "ejs");
+
 const staticPath = path.join(__dirname, "./public");
 
 app.use("/", commonRoutes);
